e895c79f403d7153bf2c59962e0f838aac6619a9
David Salazar <salazar.david.contractor@bbva.com>
2021-04-04 17:13:49 -0500

    feat(*): metodos desde services memo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MemoServiceService } from './services/memo-service.service';
 
 @Component({
   selector: 'app-root',
@@ -9,38 +10,14 @@ export class AppComponent {
   title = 'memotest';
 
   estado = [];
-  num_imagenes = 7;
-  ditribucion_cartas = []
+  distribucion_cartas = []
 
-  constructor(){
-    this.ditribucion_cartas = this.getRandomDistribution();
+  constructor(private memo: MemoServiceService){
+    this.distribucion_cartas = this.memo.getRandomDistribution();
   }
 
   muestraAlerta(numImage) {
     window.alert(numImage);
-  }
-
-  randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  getRandomDistribution(): number[] {
-    let orderedList = []
-    for (let i = 0; i < this.num_imagenes; i++) {
-      orderedList.push(i)
-      orderedList.push(i)
-    }
-    console.log(orderedList)
-    let unorderedList = []
-    let iter = 2 * this.num_imagenes
-    let _iter = iter
-    for (let i = 0; i < iter; i++) {
-      let index = this.randomIntFromInterval(0, _iter - 1)
-      unorderedList.push(orderedList[index])
-      _iter--
-      orderedList.splice(index, 1);
-    }
-    return unorderedList
   }
 
   actualizaEstado(numImage) {
@@ -51,7 +28,7 @@ export class AppComponent {
       // no detecta cambios que se produzcan en un array (añadir o quitar elementos),
       // hay que reasignar el array completo.
       // Por eso se crea una copia temporal del array de estados y se
-      // reasigna. 
+      // reasigna.
       let _estado = JSON.parse(JSON.stringify(this.estado))
       this.estado = _estado
     }
--- src/app/carta/carta.component.ts
+++ src/app/carta/carta.component.ts
@@ -1,15 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
-const imagenes = [
-  "https://image.freepik.com/free-vector/vector-illustration-cosmonaut_1441-11.jpg",
-  "https://image.freepik.com/free-vector/polygonal-lion-head_23-2147495868.jpg",
-  "https://image.freepik.com/free-vector/hand-painted-steampunk-man-illustration_23-2147537528.jpg",
-  "https://image.freepik.com/free-vector/analytical-and-creative-brain_23-2147506845.jpg",
-  "https://image.freepik.com/free-vector/abstract-floral-background_1005-10.jpg",
-  "https://image.freepik.com/free-vector/thank-you-composition-in-comic-style_23-2147831785.jpg",
-  "https://media.giphy.com/media/3ornk2zKMUgETO4aMo/giphy.gif"
-]
-
+import { MemoServiceService } from '../services/memo-service.service';
 @Component({
   selector: 'app-carta',
   templateUrl: './carta.component.html',
@@ -19,26 +9,19 @@ export class CartaComponent implements OnInit {
 
   // La imagen que se muestra
   @Input('numImage') numImage: number;
-
   @Input('estado') estado: number[];
-
   @Output('onChangeState') onChangeState = new EventEmitter<number>()
-
   resolved = false;
-
   urlImage: string;
-
   // La imagen de atrás, común a todas las "cartas"
   urlImageBack = "https://image.freepik.com/vector-gratis/confeti-dorado-diseno-festivo_53876-67683.jpg";
 
-  constructor() {
+  constructor(private memo: MemoServiceService) {
   }
 
   ngOnInit() {
     this.urlImage = this.urlImageBack;
   }
-
-
   ngOnChanges() {
     console.log(this.estado);
     if(this.resolved) return;
@@ -49,15 +32,14 @@ export class CartaComponent implements OnInit {
       }else if(this.estado[0] != this.estado[1] && (this.numImage == this.estado[0] || this.numImage == this.estado[1]) ){
         setTimeout(() => {
           this.urlImage = this.urlImageBack;
-        }, 500);  
+        }, 500);
       }
-    } 
+    }
   }
-
   flip() {
     // Cuando el usuario pica en la carta hay que darle la vuelta, es decir,
     // mostrar la imagen que le corresponde al índice `n`
-    this.urlImage = imagenes[this.numImage];
+    this.urlImage = this.memo.getImage(this.numImage);
     this.onChangeState.emit(this.numImage);
   }
 
